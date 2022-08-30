df555e97ca0357ac933a1514c318f9827896cda7
PyBaker <75681212+PyBaker@users.noreply.github.com>
2022-08-30 11:26:24 +0200

    hotfix

--- 0-pascal_triangle.py
+++ 0-pascal_triangle.py
@@ -1,15 +1,23 @@
+#!/usr/bin/python3
+"""
+Pascal triangle module
+"""
+
+
 def pascal_triangle(n):
     """ Function to return Pascal's Triangle """
     if n <= 0:
         return []
-    pascal_list = []
-    print(pascal_list)
-    for i in range(n,-1,-1):
+    pascal_list = [[]]
+    for i in range(n):
         if i == n:
             continue
-        pascal_list.insert(0,i)
-        pascal_list.insert(-1,i)
-        print(pascal_list)
+        while len(pascal_list) != n:
+            previous_row = pascal_list[-1]
+            current_row = [1]
+            for i in range(len(previous_row) - 1):
+                current_row.append(previous_row[i] + previous_row[i + 1])
+            current_row.append(1)
+            pascal_list.append(current_row)
     return pascal_list
 
-print(pascal_triangle(5))
