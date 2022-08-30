abec160717b7360a9056ecd53e2a7023b9c3dbbf
PyBaker <75681212+PyBaker@users.noreply.github.com>
2022-08-30 11:06:22 +0200

    hotfix

--- 0-pascal_triangle.py
+++ 0-pascal_triangle.py
@@ -3,10 +3,13 @@ def pascal_triangle(n):
     if n <= 0:
         return []
     pascal_list = []
-    for i in range(0,n,-1):
+    print(pascal_list)
+    for i in range(n,-1,-1):
         if i == n:
             continue
-        pascal_list[0].append(i)
-        pascal_list[-1].append(i)
+        pascal_list.insert(0,i)
+        pascal_list.insert(-1,i)
+        print(pascal_list)
     return pascal_list
 
+print(pascal_triangle(5))
