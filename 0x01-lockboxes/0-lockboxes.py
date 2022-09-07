01b6a6e8255c2237efa1e269d58f433a7c372332
PyBaker <75681212+PyBaker@users.noreply.github.com>
2022-09-07 20:59:07 +0200

    added task

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
+
 """
 Write a method that determines if all the boxes can be opened.
-where A key with the same number as a box opens that box
+
+Prototype: def canUnlockAll(boxes)
+boxes is a list of lists
+A key with the same number as a box opens that box
+You can assume all keys will be positive integers
+There can be keys that do not have boxes
+The first box boxes[0] is unlocked
+Return True if all boxes can be opened, else return False
 """
 
 
---
+++ 0x02-minimum_operations/README.md
@@ -0,0 +1,1 @@
+This is a module fewest number of operations needed
